a0f4fc54d1f145fcb04a05c07f058fe61554c2eb
Michael Anderson <Micmail566@gmail.com>
2017-03-02 09:32:34 -0500

    Add project files.

---
+++ CraigslistbyMic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CraigslistbyMic.Startup))]
+namespace CraigslistbyMic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
